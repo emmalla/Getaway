c247a901ef9e76872ff6dd8161880f00136a35b4
Emily Mallaber <emmalla@bu.edu>
2019-12-07 18:57:37 -0500

    make sure i didnt break it push

--- backend/API/routes/flights.js
+++ backend/API/routes/flights.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 var request = require("request");
 
-
+// make sure all is well push
 
 
 router.get('/:destination', (req, res, next) => {

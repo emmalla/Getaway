61a52da94f22c1141d2678c6c0a959fa959e48a9
Emily Mallaber <emmalla@bu.edu>
2019-12-07 22:44:14 -0500

    fixed cors i hope

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const app = express();
 
-//const cors = require('cors');
+const cors = require('cors');
 const port = 3000;
 
 app.listen(port);
@@ -14,7 +14,7 @@ const callbacks = require('./API/routes/callback');
 const where2 = require('./API/routes/where');
 const airport = require('./API/routes/airport');
 
-//app.use(cors);
+app.use(cors());
 app.use('/flights', flightRoutes);
 app.use('/signin', OAuth);
 app.use('/callback', callbacks);

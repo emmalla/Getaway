469372b40395a599b9fd67ce865a3ab4019ff6d2
Emily Mallaber <emmalla@bu.edu>
2019-12-08 17:22:20 -0500

    no cors error :)

--- backend/API/routes/flights.js
+++ backend/API/routes/flights.js
@@ -5,6 +5,39 @@ var request = require("request");
 
 var code = "";
 
+/* router.get('/LHR', (req, res, next) => {
+    console.log(code)
+    console.log("got here")
+    var go_url =  'https://api.flightstats.com/flex/schedules/rest/v1/json/from/BOS/to/LHR/departing/2019/12/11'
+    var options = {
+        method: 'GET',
+        url: go_url,
+        qs:
+        {
+            appId: 'b8b20992',
+            appKey: 'c2a78b3617c7e40265cb7c06e964aa65'
+        },
+        headers:
+        {
+            'cache-control': 'no-cache',
+            Connection: 'keep-alive',
+            Host: 'api.flightstats.com',
+            'Postman-Token': 'b8077019-7177-42e9-b74a-142b5d8de866,b4c73878-8aab-4ac1-b612-8622d84308b2',
+            'Cache-Control': 'no-cache',
+            
+        },
+        json:true
+    };
+
+    request(options, function (error, response, body) {
+        if (error) throw new Error(error);
+
+        console.log(body);
+        res.status(200).json(body)
+    });
+});
+ */
+
 router.get('/',(req, res, next) => {
   console.log(req.session.id)
 
--- backend/server.js
+++ backend/server.js
@@ -19,11 +19,9 @@ const flightRoutes = require('./API/routes/flights');
 const OAuth = require('./API/routes/google');
 const callbacks = require('./API/routes/callback');
 const where2 = require('./API/routes/where');
-const airport = require('./API/routes/airport');
 
 
 app.use('/flights', flightRoutes);
 app.use('/signin', OAuth);
 app.use('/callback', callbacks);
 app.use('/where', where2);
-app.use('/airport', airport);

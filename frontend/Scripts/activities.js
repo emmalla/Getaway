2550ebcd23c7611d119fdd84f54f5faea9e5f4d2
chrisr98 <craymond098@gmail.com>
2019-12-09 01:13:43 -0500

    more changes

---
+++ frontend/Scripts/activities.js
@@ -0,0 +1,71 @@
+document.getElementById("nature").onclick = function() {
+    $.getJSON( "http://localhost:3000/where/nature", function( data ) {
+        document.getElementById("Header").innerHTML = "Nature";
+        name = data.name
+        var placeToVisit = document.createElement("P");
+        placeToVisit.innerHTML = "You should visit \"" + name + "\"";
+        document.getElementById("Info").innerHTML = ""
+        document.getElementById("Info").appendChild(placeToVisit);
+        console.log(data.name);
+
+
+        var x = document.getElementById("goButton");
+        x.style.display = "block";
+
+    });
+}
+
+
+
+document.getElementById("museums").onclick = function() {
+    $.getJSON( "http://localhost:3000/where/museums", function( data ) {
+        document.getElementById("Header").innerHTML = "Museums";
+        name = data.name
+        var placeToVisit = document.createElement("P");
+        placeToVisit.innerHTML = "You should check out \"" + name + "\"";
+        document.getElementById("Info").innerHTML = ""
+        document.getElementById("Info").appendChild(placeToVisit);
+        console.log(data.name);
+
+
+    });
+}
+
+document.getElementById("shows").onclick = function() {
+    $.getJSON( "http://localhost:3000/where/shows", function( data ) {
+        document.getElementById("Header").innerHTML = "Entertainment";
+        name = data.name
+        var placeToVisit = document.createElement("P");
+        placeToVisit.innerHTML = "You should look up \"" + name + "\"";
+        document.getElementById("Info").innerHTML = ""
+        document.getElementById("Info").appendChild(placeToVisit);
+        console.log(data.name);
+
+    });
+}
+
+document.getElementById("history").onclick = function() {
+    $.getJSON( "http://localhost:3000/where/history", function( data ) {
+        document.getElementById("Header").innerHTML = "History";
+        name = data.name
+        var placeToVisit = document.createElement("P");
+        placeToVisit.innerHTML = "You should go to \"" + name + "\"";
+        document.getElementById("Info").innerHTML = ""
+        document.getElementById("Info").appendChild(placeToVisit);
+        console.log(data.name);
+
+    });
+}
+
+document.getElementById("active").onclick = function() {
+    $.getJSON( "http://localhost:3000/where/active", function( data ) {
+        document.getElementById("Header").innerHTML = "Active";
+        name = data.name
+        var placeToVisit = document.createElement("P");
+        placeToVisit.innerHTML = "You should look for \"" + name + "\"";
+        document.getElementById("Info").innerHTML = ""
+        document.getElementById("Info").appendChild(placeToVisit);
+        console.log(data.name);
+
+    });
+}
---
+++ frontend/Scripts/callFlightAPI.js
@@ -0,0 +1,40 @@
+$.getJSON( "http://localhost:3000/flights/LHR", function( data ) {
+    document.getElementById("Header").innerHTML = "Flight Info";
+
+    for (var i = 0; i < 15; i++) {
+        departureTime = data.AirlineFlightSchedulesResult.data[i].departuretime
+        var dtimep = document.createElement("P");
+        dtimep.innerHTML = "Departure time is " + departureTime;
+        departureTerminal = data.AirlineFlightSchedulesResult.data[i].origin
+        var dterminalp = document.createElement("P");
+        dterminalp.innerHTML = "Departure terminal is " + departureTerminal;
+        arrivalTime = data.AirlineFlightSchedulesResult.data[i].arrivaltime
+        var atimep = document.createElement("P");
+        atimep.innerHTML = "Arrival time is " + arrivalTime;
+        arrivalTerminal = data.AirlineFlightSchedulesResult.data[i].destination
+        var aterminalp = document.createElement("P");
+        aterminalp.innerHTML = "Arrival terminal is " + arrivalTerminal;
+
+
+        var separator = document.createElement("P");
+        separator.innerHTML = "__________________________________________";
+
+        document.getElementById("Info").appendChild(dtimep);
+        document.getElementById("Info").appendChild(dterminalp);
+        document.getElementById("Info").appendChild(atimep);
+        document.getElementById("Info").appendChild(aterminalp);
+        document.getElementById("Info").appendChild(separator)
+
+
+
+
+        console.log("1 " + data.AirlineFlightSchedulesResult.data[i].departuretime);
+        console.log("2 " + data.AirlineFlightSchedulesResult.data[i].origin);
+        console.log("3 " + data.AirlineFlightSchedulesResult.data[i].arrivaltime);
+        console.log("4 " + data.AirlineFlightSchedulesResult.data[i].destination);
+
+    }
+
+console.log(data);
+
+});
